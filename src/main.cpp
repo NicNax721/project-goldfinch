70469ce944429004eef43317f30433e628fc5310
NicNax721 <23101229@usc.edu.ph>
2026-08-25 23:54:20 +0800

    feat(ui_design): added life to the window through themes.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,13 @@
 int main(int argc, char* argv[]) {
   QApplication app(argc, argv);
 
+  // ---- Window UI theme using style.qss (I fabricated this with Claude Design) ----
+  app.setStyle(QStyleFactory::create("Fusion"));
+
+  QFile f(":/resources/style.qss");
+  if (f.open(QIODevice::ReadOnly | QIODevice::Text))
+    app.setStyleSheet(QString::fromUtf8(f.readAll()));
+
   MainWindow window;
   window.show();
 
